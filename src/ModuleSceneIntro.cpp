cf590ef28c4a7767b75ca98e33232bd269173488
Carlos <arnau.carlos01@gmail.com>
2022-10-04 09:11:30 +0200

    Changing cubshit

--- src/ModuleSceneIntro.cpp
+++ src/ModuleSceneIntro.cpp
@@ -2,10 +2,16 @@
 #include "Application.h"
 #include "ModuleSceneIntro.h"
 #include "Primitive.h"
+#include "SDL_opengl.h"
+#include <gl/GL.h>
+#include <gl/GLU.h>
+
 #include "imgui.h"
 #include "imgui_impl_opengl3.h"
 #include "imgui_impl_sdl.h"
 
+#pragma comment (lib, "glu32.lib")    /* link OpenGL Utility lib     */
+#pragma comment (lib, "opengl32.lib") /* link Microsoft OpenGL lib   */
 
 ModuleSceneIntro::ModuleSceneIntro(Application* app, bool start_enabled) : Module(app, start_enabled)
 {
@@ -52,6 +58,52 @@ update_status ModuleSceneIntro::Update(float dt)
 	Plane p(0, 1, 0, 0);
 	p.axis = true;
 	p.Render();
+
+    // Render a color-cube consisting of 6 quads with different colors
+    glBegin(GL_QUADS);                // Begin drawing the color cube with 6 quads
+       // Top face (y = 1.0f)
+       // Define vertices in counter-clockwise (CCW) order with normal pointing out
+    glColor3f(0.0f, 1.0f, 0.0f);     // Green
+    glVertex3f(1.0f, 2.0f, -1.0f);
+    glVertex3f(-1.0f, 2.0f, -1.0f);
+    glVertex3f(-1.0f, 2.0f, 1.0f);
+    glVertex3f(1.0f, 2.0f, 1.0f);
+
+    // Bottom face (y = -1.0f)
+    glColor3f(1.0f, 0.5f, 0.0f);     // Orange
+    glVertex3f(1.0f, 0.0f, 1.0f);
+    glVertex3f(-1.0f, 0.0f, 1.0f);
+    glVertex3f(-1.0f, 0.0f, -1.0f);
+    glVertex3f(1.0f, 0.0f, -1.0f);
+
+    // Front face  (z = 1.0f)
+    glColor3f(1.0f, 1.0f, 0.0f);     // Red
+    glVertex3f(1.0f, 2.0f, 1.0f);
+    glVertex3f(-1.0f, 2.0f, 1.0f);
+    glVertex3f(-1.0f, 0.0f, 1.0f);
+    glVertex3f(1.0f, 0.0f, 1.0f);
+
+    // Back face (z = -1.0f)
+    glColor3f(1.0f, 1.0f, 0.0f);     // Yellow
+    glVertex3f(1.0f, 0.0f, -1.0f);
+    glVertex3f(-1.0f, 0.0f, -1.0f);
+    glVertex3f(-1.0f, 2.0f, -1.0f);
+    glVertex3f(1.0f, 2.0f, -1.0f);
+
+    // Left face (x = -1.0f)
+    glColor3f(0.0f, 0.0f, 1.0f);     // Blue
+    glVertex3f(-1.0f, 2.0f, 1.0f);
+    glVertex3f(-1.0f, 2.0f, -1.0f);
+    glVertex3f(-1.0f, 0.0f, -1.0f);
+    glVertex3f(-1.0f, 0.0f, 1.0f);
+
+    // Right face (x = 1.0f)
+    glColor3f(1.0f, 0.0f, 1.0f);     // Magenta
+    glVertex3f(1.0f, 2.0f, -1.0f);
+    glVertex3f(1.0f, 2.0f, 1.0f);
+    glVertex3f(1.0f, 0.0f, 1.0f);
+    glVertex3f(1.0f, 0.0f, -1.0f);
+    glEnd();  // End of drawing color-cube
 
 	if (App->debug == true)
 	{
