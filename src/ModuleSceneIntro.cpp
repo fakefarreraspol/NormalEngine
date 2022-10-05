ca11fdacc3992992097b7e76a77f3b7739103719
Carlos <arnau.carlos01@gmail.com>
2022-10-05 15:00:40 +0200

    Yes?

--- src/ModuleSceneIntro.cpp
+++ src/ModuleSceneIntro.cpp
@@ -7,16 +7,13 @@
 #include "imgui.h"
 #include "imgui_impl_opengl3.h"
 #include "imgui_impl_sdl.h"
-
-/*
 #include "cimport.h"
 #include "scene.h"
 #include "postprocess.h"
 
 #pragma comment (lib, "assimp.lib")
-*/
-
 #pragma comment (lib, "opengl32.lib") /* link Microsoft OpenGL lib   */
+
 
 ModuleSceneIntro::ModuleSceneIntro(Application* app, bool start_enabled) : Module(app, start_enabled)
 {
