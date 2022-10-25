924b08efdc0cbc27dbb3624e6172e74ca86e889e
carlosarnau <arnau.carlos01@gmail.com>
2022-10-25 15:57:51 +0200

    Implementing mouse wheel functionality
    ...

--- NormalEngine/ModuleCamera3D.cpp
+++ NormalEngine/ModuleCamera3D.cpp
@@ -44,7 +44,7 @@ update_status ModuleCamera3D::Update(float dt)
 	vec3 newPos(0,0,0);
 	float speed = 3.0f * dt;
 	if(App->input->GetKey(SDL_SCANCODE_LSHIFT) == KEY_REPEAT)
-		speed = 8.0f * dt;
+		speed = 25.0f * dt;
 
 	if(App->input->GetKey(SDL_SCANCODE_R) == KEY_REPEAT) newPos.y += speed;
 	if(App->input->GetKey(SDL_SCANCODE_F) == KEY_REPEAT) newPos.y -= speed;
@@ -55,6 +55,18 @@ update_status ModuleCamera3D::Update(float dt)
 
 	if(App->input->GetKey(SDL_SCANCODE_A) == KEY_REPEAT) newPos -= X * speed;
 	if(App->input->GetKey(SDL_SCANCODE_D) == KEY_REPEAT) newPos += X * speed;
+
+	if (App->input->GetMouseZ() > 0)
+	{
+		// dezoom
+		newPos -= Z * speed;
+	}
+	if (App->input->GetMouseZ() < 0)
+	{
+		//zoom
+		newPos += Z * speed;
+
+	}
 
 	Position += newPos;
 	Reference += newPos;
